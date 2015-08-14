9209a47c2479989e94d803dd5522c1d707d0aebf
Fernando Marquardt <fernando.marquardt@gmail.com>
2015-08-14 10:22:03 -0300

    Created the logo and finished the FrontForm layout.

--- src/com/progressquest/ultra/FrontForm.java
+++ src/com/progressquest/ultra/FrontForm.java
@@ -1,15 +1,21 @@
 package com.progressquest.ultra;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
-import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.progressquest.ultra.components.ImagePanel;
 import com.progressquest.ultra.util.Resources;
+
+import net.miginfocom.swing.MigLayout;
 
 /**
  *
@@ -17,59 +23,75 @@ import com.progressquest.ultra.util.Resources;
  */
 public class FrontForm extends JFrame {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private ImagePanel logoPanel;
-	private JButton newSinglePlayerBtn;
-	private JButton newMultiplayerBtn;
-	private JButton loadGameBtn;
-	private JButton exitBtn;
+    private ImagePanel logoPanel;
+    private JButton newSinglePlayerBtn;
+    private JButton newMultiplayerBtn;
+    private JButton loadGameBtn;
+    private JButton exitBtn;
 
-	public FrontForm() {
-		initForm();
-		initComponents();
-	}
+    public FrontForm() {
+        initForm();
+        initComponents();
+    }
 
-	private void initForm() {
-		setResizable(false);
-		setTitle("Ultra ProgressQuest");
+    private void initForm() {
+        setResizable(false);
+        setTitle("Ultra Progress Quest");
 
-		setLayout(new BorderLayout(10, 0));
-		((JPanel) getContentPane()).setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-	}
+        getContentPane().setBackground(Color.WHITE);
+        getContentPane().setLayout(new BorderLayout(10, 0));
+        ((JPanel) getContentPane()).setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-	private void initComponents() {
-		logoPanel = new ImagePanel(Resources.LOGO_IMG);
+        List<Image> icons = new ArrayList<>();
+        icons.add(Resources.APP_ICON_64);
+        icons.add(Resources.APP_ICON_32);
+        icons.add(Resources.APP_ICON_16);
 
-		newSinglePlayerBtn = new JButton("New Game (Single Player)");
-		newSinglePlayerBtn.setMnemonic('N');
-		newSinglePlayerBtn.setToolTipText("Start a new single player game.");
+        setIconImages(icons);
+    }
 
-		newMultiplayerBtn = new JButton("New Game (Multiplayer)");
-		newMultiplayerBtn.setMnemonic('M');
-		newMultiplayerBtn.setEnabled(false);
-		newMultiplayerBtn.setToolTipText("Start a new multiplayer game (There is no multiplayer support right now)");
+    private void initComponents() {
+        logoPanel = new ImagePanel(Resources.LOGO_IMG);
 
-		loadGameBtn = new JButton("Load Game");
-		loadGameBtn.setMnemonic('L');
-		loadGameBtn.setToolTipText("Load a single player game");
+        newSinglePlayerBtn = new JButton("New Game (Single Player)");
+        newSinglePlayerBtn.setBackground(null);
+        newSinglePlayerBtn.setMnemonic('N');
+        newSinglePlayerBtn.setToolTipText("Start a new single player game.");
 
-		exitBtn = new JButton("Exit");
-		exitBtn.setMnemonic('x');
+        newMultiplayerBtn = new JButton("New Game (Multiplayer)");
+        newMultiplayerBtn.setBackground(null);
+        newMultiplayerBtn.setMnemonic('M');
+        newMultiplayerBtn.setEnabled(false);
+        newMultiplayerBtn.setToolTipText("Start a new multiplayer game (There is no multiplayer support right now)");
 
-		Box buttonsPanel = Box.createVerticalBox();
-		buttonsPanel.add(newSinglePlayerBtn);
-		buttonsPanel.add(Box.createVerticalGlue());
-		buttonsPanel.add(newMultiplayerBtn);
-		buttonsPanel.add(Box.createVerticalGlue());
-		buttonsPanel.add(loadGameBtn);
-		buttonsPanel.add(Box.createVerticalGlue());
-		buttonsPanel.add(exitBtn);
+        loadGameBtn = new JButton("Load Game");
+        loadGameBtn.setBackground(null);
+        loadGameBtn.setMnemonic('L');
+        loadGameBtn.setToolTipText("Load a single player game");
 
-		add(logoPanel, BorderLayout.WEST);
-		add(buttonsPanel, BorderLayout.CENTER);
+        exitBtn = new JButton("Exit");
+        exitBtn.setBackground(null);
+        exitBtn.setMnemonic('x');
 
-		pack();
-	}
+        JPanel buttonsPanel = new JPanel(new MigLayout("", "[grow]", "[grow]"));
+        buttonsPanel.setBackground(null);
+        buttonsPanel.add(newSinglePlayerBtn, "grow, wrap");
+        buttonsPanel.add(newMultiplayerBtn, "grow, wrap");
+        buttonsPanel.add(loadGameBtn, "grow, wrap");
+        buttonsPanel.add(exitBtn, "grow");
+
+        JLabel copyrightLbl = new JLabel("\u00a9 2015 Fernando Marquardt - v0.0.1");
+        JPanel copyrightPanel = new JPanel(new MigLayout("", "[grow, align center]"));
+        copyrightPanel.setBackground(null);
+        copyrightPanel.add(copyrightLbl);
+
+        add(logoPanel, BorderLayout.WEST);
+        add(buttonsPanel, BorderLayout.CENTER);
+        add(copyrightPanel, BorderLayout.SOUTH);
+
+        pack();
+    }
 
 }
--- src/com/progressquest/ultra/util/Resources.java
+++ src/com/progressquest/ultra/util/Resources.java
@@ -7,14 +7,22 @@ import javax.imageio.ImageIO;
 
 public class Resources {
 
-	public static final Image LOGO_IMG;
+    public static final Image LOGO_IMG;
 
-	static {
-		try {
-			LOGO_IMG = ImageIO.read(Resources.class.getResource("/images/logo.png"));
-		} catch (IOException e) {
-			throw new ExceptionInInitializerError(e);
-		}
-	}
+    public static final Image APP_ICON_64;
+    public static final Image APP_ICON_32;
+    public static final Image APP_ICON_16;
+
+    static {
+        try {
+            LOGO_IMG = ImageIO.read(Resources.class.getResource("/images/logo.png"));
+
+            APP_ICON_64 = ImageIO.read(Resources.class.getResource("/icons/app_icon_64.png"));
+            APP_ICON_32 = ImageIO.read(Resources.class.getResource("/icons/app_icon_32.png"));
+            APP_ICON_16 = ImageIO.read(Resources.class.getResource("/icons/app_icon_16.png"));
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
 
 }
